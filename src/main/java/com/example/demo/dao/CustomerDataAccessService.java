6a9728497282492f47fcc27bc08727a671b65077
Isaiah1114 <40809315+Isaiah1114@users.noreply.github.com>
2022-02-13 16:33:33 -0700

    Finish connections string and test post reqeust.

--- src/main/java/com/example/demo/dao/CustomerDataAccessService.java
+++ src/main/java/com/example/demo/dao/CustomerDataAccessService.java
@@ -1,16 +1,31 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Statement;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository("MYSQL")
 public class CustomerDataAccessService implements CustomerDao{
+
+    private final JdbcTemplate jdbcTemplate;
+    @Autowired
+    public CustomerDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertCustomer(UUID id, Customer customer) {
+        String sql = "insert into customers (Customer_ID, first_name, last_name, birth_date, phone, address, city, state) values(1234, 'test', 'test', '2020-1-1', 4444444444, '9999 test creek','orlando','fl');";
+        jdbcTemplate.update(sql);
+
+
+
         return 0;
     }
 
@@ -21,6 +36,7 @@ public class CustomerDataAccessService implements CustomerDao{
 
     @Override
     public List<Customer> selectAllCustomers() {
+
         String datbase = "idk";
         return List.of(new Customer(UUID.randomUUID(), datbase ));
 
--- src/main/resources/db/migration/V1__CustomerTable.sql
+++ src/main/resources/db/migration/V1__CustomerTable.sql
@@ -1,4 +1,6 @@
-Create Table test (
-    id uuid not null primary key,
-    name varchar(100) not null
-);
+use dataonlineshop;
+
+
+
+insert into customers (Customer_ID, first_name, last_name, birth_date, phone, address, city, state)
+values(1234, 'test', 'test', '2020-1-1', 4444444444, "9999 cascade creek view","orlando","fl");
