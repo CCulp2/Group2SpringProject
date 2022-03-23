dd2b1df235fd0930e79646fff166aae6212a919e
CCulp2 <80595975+CCulp2@users.noreply.github.com>
2022-03-23 18:55:13 -0400

    Creating JWT with username in it
    Creates a JWT with full username and auth for 166 hours.

--- src/main/java/com/example/demo/security/CustomAuthFilter.java
+++ src/main/java/com/example/demo/security/CustomAuthFilter.java
@@ -1,5 +1,7 @@
 package com.example.demo.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -14,6 +17,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 public class CustomAuthFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
@@ -44,6 +48,12 @@ public class CustomAuthFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+        User customer = (User) authResult.getPrincipal();
+        Algorithm algorithm = Algorithm.HMAC256("Group2Spring2022".getBytes());
+        String accessToken = JWT.create()
+                .withSubject(customer.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000 * 1000))
+                .withIssuer(request.getRequestURL().toString())
+                .sign(algorithm);
     }
 }
