1f51ced2f958e1189d64587d55d346b978c1d7f9
CCulp2 <80595975+CCulp2@users.noreply.github.com>
2022-04-14 18:41:00 -0400

    Products autoload on startup
    Products now auto-populate the database when coming back from a zero start.

--- src/main/java/com/example/demo/CapstoneProjectApplication.java
+++ src/main/java/com/example/demo/CapstoneProjectApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
 import com.example.demo.model.Customer;
+import com.example.demo.model.Product;
+import com.example.demo.model.ProductLoadData;
 import com.example.demo.service.CustomerService;
+import com.example.demo.service.ProductService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.ResourceUtils;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class CapstoneProjectApplication {
@@ -17,13 +28,24 @@ public class CapstoneProjectApplication {
 	}
 
 	@Bean
-	CommandLineRunner run(CustomerService service) {
+	CommandLineRunner run(CustomerService customerService, ProductService productService) {
+		Resource productsJSON = new ClassPathResource("productsList.JSON");
 		return args -> {
-			if (service.getCustomerByUsername("ADMIN").isEmpty()) {
+			if (customerService.getCustomerByUsername("ADMIN").isEmpty()) {
 				Customer admin = new Customer();
 				admin.setUsername("ADMIN");
 				admin.setPassword("PASSWORD");
-				service.addCustomer(admin);
+				customerService.addCustomer(admin);
+				ObjectMapper map = new ObjectMapper();
+				TypeReference<List<Product>> typeReference = new TypeReference<List<Product>>(){};
+				InputStream inputStream = productsJSON.getInputStream();
+				try {
+					List<Product> products = map.readValue(inputStream, typeReference);
+					productService.addProducts(products);
+				} catch (IOException e) {
+					System.out.println("Failed to log products because");
+					System.out.println(e);
+				}
 			}
 		};
 	}
